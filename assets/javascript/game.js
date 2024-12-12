2ff21a77573958bd5b97d020b615d3eb2a5c04d3
CaoKao <bksaechao@gmail.com>
2024-12-12 00:19:41 -0800

    restructured gundam build methods for cleaner code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,24 +26,72 @@ let gundamFighter = {
     },
   },
 
+  // creates image and container for gundams to be displayed
+  buildGundam: function (key) {
+    // gundam container
+    let gundamDiv = $("<div>");
+    gundamDiv.attr("class", "gundam-container");
+    gundamDiv.html(
+      "<h1 class='gundam-name'>" +
+        key.toUpperCase() +
+        "</h1>" +
+        "<h1 class='gundam-health'>" +
+        gundamFighter.gundams[key].health +
+        "</h1>"
+    );
+    // gundam image
+    let heroImg = $("<img>");
+    heroImg.attr({
+      src: gundamFighter.gundams[key].image,
+      alt: key,
+      class: "gundam-img",
+      value: key,
+    });
+    heroImg.css({ height: "300px", width: "225px" });
+    gundamDiv.append(heroImg);
+    return gundamDiv;
+  },
+
+  buildEnemyGundam: function (key) {
+    // gundam container
+    let gundamDiv = $("<div>");
+    gundamDiv.attr("class", "gundam-container");
+    gundamDiv.html(
+      "<h1 class='gundam-name'>" +
+        key.toUpperCase() +
+        "</h1>" +
+        "<h1 class='gundam-health'>" +
+        gundamFighter.gundams[key].health +
+        "</h1>"
+    );
+    // gundam image
+    let heroImg = $("<img>");
+    heroImg.attr({
+      src: gundamFighter.gundams[key].image,
+      alt: key,
+      class: "enemy-gundam-img",
+      value: key,
+    });
+    heroImg.css({ height: "300px", width: "225px" });
+    gundamDiv.append(heroImg);
+    return gundamDiv;
+  },
+
   // displays gundams to page
   displayGundams: function () {
     $.each(this.gundams, function (key) {
-      // gundam container
-      let gundamDiv = $("<div>");
-      gundamDiv.attr("class", "gundam-container");
-      gundamDiv.html("<h1 class='gundam-name'>" + key.toUpperCase() + "</h1>");
-      // gundam image
-      let heroImg = $("<img>");
-      heroImg.attr({
-        src: gundamFighter.gundams[key].image,
-        alt: key,
-        class: "gundam-img",
-        value: key,
-      });
-      heroImg.css({ height: "300px", width: "225px" });
-      gundamDiv.append(heroImg);
-      $("#characters").append(gundamDiv);
+      $("#character-select").append(gundamFighter.buildGundam(key));
+    });
+  },
+
+  characterSelect: function (userPick) {
+    $("#character-select").empty();
+    $.each(this.gundams, function (key) {
+      if (userPick === key) {
+        $("#arena").append(gundamFighter.buildGundam(key));
+      } else {
+        $("#enemies").append(gundamFighter.buildEnemyGundam(key));
+      }
     });
   },
 };
@@ -52,7 +100,12 @@ gundamFighter.displayGundams();
 
 $(document).ready(function () {
   $(".gundam-img").click(function (e) {
-    // $("#character-select").empty();
-    console.log(e.target.alt);
+    let userPick = e.target.alt;
+    console.log(userPick);
+    gundamFighter.characterSelect(userPick);
+  });
+
+  $(".enemy-gundam-img").click(function (e) {
+    let userPick = e.target.alt;
   });
 });
